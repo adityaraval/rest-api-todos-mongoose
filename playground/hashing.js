c4da77d8d05c8a848c1091963ee20c6b7ee3d2d0
Aditya Raval <adi7017@gmail.com>
2017-01-10 21:54:10 +0530

    Added Hashing Password Mechanism

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,20 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password = '123abc!';
+/*
+bcrypt.genSalt(10,(err,salt)=>{
+  bcrypt.hash(password,salt,(err,hash)=>{
+    console.log(hash);
+  });
+});
+*/
+var hashed = '$2a$10$TfqcRHBqgLwdJG2GkZnooua/5AI3GG9qZHux5K0mf/vy1BPMAMOiG';
+bcrypt.compare(password,hashed,(err,result)=>{
+  console.log(result);
+});
+
 /*
 var message = "Hello world";
 var hash = SHA256(message).toString();
@@ -7,6 +22,7 @@ console.log(`Message:${message}`);
 console.log(`Hash:${hash}`);
 */
 
+/*
 var data = {
   id:5
 }
@@ -16,3 +32,4 @@ console.log(token)
 
 var decoded = jwt.verify(token,'abc123');
 console.log("Decoded",decoded)
+*/
